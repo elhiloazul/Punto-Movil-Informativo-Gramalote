d53ad13a8f88963efb5d0fcb9cf501637794066b
elhiloazul <operativo.elhiloazul@gmail.com>
2025-09-05 17:28:07 -0500

    Se crea componente incial y se hace conexion a openai para la voz

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { OpenaiService } from '../../services/openai.service';
+import { time } from 'console';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   imports: [
-    CommonModule, 
-    RouterLink, 
-    
+    CommonModule,
+    MatIconModule,
+    RouterLink
+
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private openaiService: OpenaiService) { }
+
+
+  ngOnInit(): void {
+    //this.hablarOpenAI("Bienvenido al punto móvil informativo de Gramalote. Aquí encontrarás información relevante sobre nuestra comunidad, eventos y servicios disponibles. Explora las diferentes secciones para mantenerte informado y conectado con lo que sucede en Gramalote. ¡Gracias por visitarnos!");
+  }
+
+  hablarOpenAI(text: string) {
+    this.openaiService.speakDirect(text).then(blob => {
+      const url = URL.createObjectURL(blob);
+      const audio = new Audio(url);
+      audio.play();
+    });
+  }
+
+  speak(text: string, callback?: () => void) {
+    speechSynthesis.cancel();
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = "es-419";
+    utterance.rate = 1; 
+    utterance.pitch = 1;
+
+    utterance.onend = () => {
+      console.log("✅ Terminó de hablar");
+      if (callback) {
+        callback(); 
+      }
+    };
+
+    speechSynthesis.speak(utterance);
+  }
+
+  clickSound() {
+    this.speak("Hola, soy Tico, un mono Titi que está emocionado por conocerte. Estoy muy feliz de que estés aquí, y tengo mucho que contarte, pero primero, aprendamos a usar la pantalla", () => {
+      console.log("👉 Ahora puedo pasar al siguiente paso del tutorial");
+    });
+  }
+
+
 
 }
