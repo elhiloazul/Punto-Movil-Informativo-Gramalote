d600caa01ed6f28df7ccee773f3f217d5fdf9a74
jsandoval86 <juancsandoval86@gmail.com>
2026-01-12 14:17:26 -0500

    # 5.1: Improve navigation, stop audio when component out

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -10,55 +10,61 @@ export const ACTIVITIES: Activity[] = [
                 id: 'slide-1',
                 type:'text',
                 text: '¡Perfecto! bienvenido a conocer un poco de la historia de Gramalote.',
-                audio: 'audio/actividades/modulo-1/slide-1.mp3'
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-2',
                 type: 'text',
                 text: '¿Sabes que es una licencia ambiental?',
-                audio: 'audio/actividades/modulo-1/slide-2.mp3'
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-3',
                 type: 'image',
-                imageUrl: 'assets/images/actividad1/modulo1-slide3.png',
+                imageUrl: 'images/actividades/modulo-1/slide-3.png',
                 text: 'Ubicado en Antioquia en el corregimiento Providencia, de San Roque; hace 20 años se estudió el lugar para desarrollar un proyecto minero,',
-                audio: 'audio/actividades/modulo-1/slide-1.mp3'
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-4',
                 type: 'text',
-                text: 'la calidad del oro e impacto ambiental en el área del territorio de San Roque y Yolombó donde se adelantaron estudios acerca de la mejor forma de construir y operar el proyecto, que diera beneficios a la región. (2022 – 2025)'
+                text: 'la calidad del oro e impacto ambiental en el área del territorio de San Roque y Yolombó donde se adelantaron estudios acerca de la mejor forma de construir y operar el proyecto, que diera beneficios a la región. (2022 – 2025)',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-5',
                 type: 'image',
                 imageUrl: 'assets/images/actividad1/modulo1-slide5.png',
-                text: 'dando como resultado, una mina moderna con una vida útil de 13 años,'
+                text: 'dando como resultado, una mina moderna con una vida útil de 13 años,',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-6',
                 type: 'image',
                 imageUrl: 'assets/images/actividad1/modulo1-slide6.png',
-                text: 'la cual posee licencia ambiental desde el 2015, pero debes tener presente que en este momento el proyecto Gramalote, está modificando su licencia ambiental; si quieres saber por qué, ¡explora un poco más!'
+                text: 'la cual posee licencia ambiental desde el 2015, pero debes tener presente que en este momento el proyecto Gramalote, está modificando su licencia ambiental; si quieres saber por qué, ¡explora un poco más!',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-7',
                 type: 'image',
                 imageUrl: 'assets/images/actividad1/modulo1-slide7.png',
-                text: 'Mientras se realiza este proceso, se trabaja de la mano con la comunidad contribuyendo en su desarrollo en salud, seguridad, productividad, y medio ambiente. Además, se adelantan procesos de reasentamiento y formalización minera'
+                text: 'Mientras se realiza este proceso, se trabaja de la mano con la comunidad contribuyendo en su desarrollo en salud, seguridad, productividad, y medio ambiente. Además, se adelantan procesos de reasentamiento y formalización minera',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-8',
                 type: 'video',
                 videoUrl: 'assets/videos/actividad1/modulo1-slide8.mp4',
-                text: 'Para conocer un poco más de esto, acompáñame a ver "presente y futuro" un video que nos muestra los aportes que el proyecto ha realizado a la comunidad'
+                text: 'Para conocer un poco más de esto, acompáñame a ver "presente y futuro" un video que nos muestra los aportes que el proyecto ha realizado a la comunidad',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             },
             {
                 id: 'slide-9',
                 type: 'video',
                 videoUrl: 'assets/videos/actividad1/modulo1-slide9.mp4',
-                text: 'Por cierto... Sabes cómo funciona la minería? Ven y averigüémoslo juntos con este video'
+                text: 'Por cierto... Sabes cómo funciona la minería? Ven y averigüémoslo juntos con este video',
+                audio: 'audio/actividades/modulo-1/slide-3.mp3'
             }
         ],
     },
