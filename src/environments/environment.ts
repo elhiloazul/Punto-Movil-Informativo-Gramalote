7a492ea2ec588644c3b5988c0d269c97d92c90b5
jsandoval86 <juancsandoval86@gmail.com>
2026-01-12 15:55:42 -0500

    #7: Add SlideVideoComponent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     baseUrlAngular: 'http://localhost:4200',
     apiKeyOpenAI: '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',
     production: false,
-    logLevel: 'DEBUG' as const
+    logLevel: 'DEBUG' as const,
+    youtubeControls: 1 as const
 };
