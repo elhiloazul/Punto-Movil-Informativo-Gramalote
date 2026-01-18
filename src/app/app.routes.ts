c9fb677561f4dde38bef7275f65b530f14d6a561
elhiloazul <operativo.elhiloazul@gmail.com>
2026-01-17 23:40:39 -0500

    Se devuelven los cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ import { ActivityOrchestratorComponent } from './pages/activity-orchestrator/act
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
-  {
-    path: 'activity/:id',
-    component: ActivityOrchestratorComponent,
-    data: { renderMode: 'client' },
-  },
-  { path: '**', redirectTo: 'home' },
+  { path: 'activity/:id', component: ActivityOrchestratorComponent },
+  { path: '**', redirectTo: 'home' }
 ];
