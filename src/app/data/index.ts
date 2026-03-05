bfab5aa47573da99f57126825d945ccba570c1d6
elhiloazul <operativo.elhiloazul@gmail.com>
2026-03-05 09:02:59 -0500

    Add animations support and migrate large GIF files to Git LFS

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -12,6 +12,7 @@ export const ACTIVITIES: Activity[] = [
         type: 'text',
         text: '¡Perfecto!, bienvenido a  conocer un poco de la historia de Gramalote.',
         audio: 'audio/actividades/modulo-1/slide-1.mp3',
+        backgroundImage: 'images/fondos/1.gif',
       },
       {
         id: 'slide-2',
@@ -19,12 +20,14 @@ export const ACTIVITIES: Activity[] = [
         imageUrl: 'images/actividades/modulo-1/slide-2.png',
         text: 'Ubicado en Antioquia en el corregimiento Providencia, en San Roque; desde hace 20 años se viene analizando la zona para desarrollar un proyecto minero.',
         audio: 'audio/actividades/modulo-1/slide-2.mp3',
+        backgroundImage: 'images/fondos/2.gif',
       },
       {
         id: 'slide-3',
         type: 'text',
         text: 'La calidad del oro y la seguridad del medio ambiente en el área del territorio de San Roque y Yolombó donde se adelantaron estudios acerca de la mejor forma de construir y operar el proyecto, que diera beneficios a la región. (2022 – 2025).',
         audio: 'audio/actividades/modulo-1/slide-3.mp3',
+        backgroundImage: 'images/fondos/3.gif',
       },
       {
         id: 'slide-4',
@@ -32,6 +35,7 @@ export const ACTIVITIES: Activity[] = [
         imageUrl: 'images/actividades/modulo-1/slide-4.png',
         text: 'Dando como resultado, una mina moderna con una vida útil de 13 años.',
         audio: 'audio/actividades/modulo-1/slide-4.mp3',
+        backgroundImage: 'images/fondos/4.gif',
       },
       {
         id: 'slide-5',
@@ -39,6 +43,7 @@ export const ACTIVITIES: Activity[] = [
         imageUrl: 'images/actividades/modulo-1/slide-5.png',
         text: 'La cual posee licencia ambiental desde el 2015, ¡y te cuento! que en este momento el proyecto Gramalote, la está modificando; si quieres saber por qué, ¡explora un poco más!.',
         audio: 'audio/actividades/modulo-1/slide-5.mp3',
+        backgroundImage: 'images/fondos/9.gif',
       },
       {
         id: 'slide-6',
@@ -46,6 +51,7 @@ export const ACTIVITIES: Activity[] = [
         imageUrl: 'images/actividades/modulo-1/slide-6.png',
         text: 'Mientras se realiza este proceso, se trabaja de la mano con la comunidad contribuyendo en su desarrollo en salud, seguridad, productividad, y medio ambiente. Además, se adelantan procesos de reasentamiento y formalización minera, todo esto con la supervisión de las Corporaciones Autónomas Regionales y las de Desarrollo Sostenible, de los municipios, distritos y áreas metropolitanas.',
         audio: 'audio/actividades/modulo-1/slide-6.mp3',
+        backgroundImage: 'images/fondos/10.gif',
       },
       {
         id: 'slide-7',
@@ -53,6 +59,7 @@ export const ACTIVITIES: Activity[] = [
         videoUrl: 'https://www.youtube.com/watch?v=cR8T-S8zfNw',
         text: 'Para conocer un poco más de esto, acompáñame a ver “presente y futuro” un video que nos muestra los aportes que el proyecto ha realizado a la comunidad',
         audio: 'audio/actividades/modulo-1/slide-7.mp3',
+        backgroundImage: 'images/fondos/7.gif',
       },
       {
         id: 'slide-8',
@@ -60,6 +67,7 @@ export const ACTIVITIES: Activity[] = [
         videoUrl: 'https://www.youtube.com/watch?v=TdkK9xpTJTw',
         text: 'Por cierto… ¿Sabes cómo funciona la minería? Ven y averigüémoslo juntos con este video.',
         audio: 'audio/actividades/modulo-1/slide-8.mp3',
+        backgroundImage: 'images/fondos/12.gif',
       }
     ],
   },
@@ -72,6 +80,7 @@ export const ACTIVITIES: Activity[] = [
         type: 'text',
         text: '¿Sabes qué es una licencia ambiental?',
         audio: 'audio/actividades/modulo-2/slide-1.mp3',
+        backgroundImage: 'images/backgrounds/licencia-ambiental-bg.jpg',
       },
       {
         id: 'slide-2',
