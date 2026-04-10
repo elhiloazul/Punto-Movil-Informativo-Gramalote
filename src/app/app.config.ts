db00807b8833dd13599801578fe09fbcd4c52491
jsandoval86 <juancsandoval86@gmail.com>
2026-04-10 10:51:37 -0500

    build: disable ssr and prerender

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { apiKeyInterceptor } from './core/interceptors/api-key.interceptor';
 import { apiResponseInterceptor } from './core/http/api-response.interceptor';
 
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     provideHttpClient(withFetch(), withInterceptors([apiKeyInterceptor, apiResponseInterceptor])),
   ],
