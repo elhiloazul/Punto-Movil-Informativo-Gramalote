45316ab1316d40874d7621caa8b63f40efde8a45
elhiloazul <operativo.elhiloazul@gmail.com>
2026-04-16 23:53:22 -0500

    Se agrega nuevos slides y se cambian algunas animaciones

--- src/app/components/slide-custom-component/slide-contact-form/slide-contact-form.component.ts
+++ src/app/components/slide-custom-component/slide-contact-form/slide-contact-form.component.ts
@@ -1,7 +1,9 @@
-﻿import { Component, EventEmitter, Output } from '@angular/core';
+﻿import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { InteractiveSlide } from '../interactive-slide';
 import { CommonModule } from '@angular/common';
+import { CustomSlide } from '../../../models/slide.model';
+import { LoggerService } from '../../../core/logger/logger.service';
 
 @Component({
   selector: 'app-slide-contact-form',
@@ -10,16 +12,74 @@ import { CommonModule } from '@angular/common';
   styleUrl: './slide-contact-form.component.scss',
 })
 export class SlideContactFormComponent implements InteractiveSlide {
+  @Input() slide?: CustomSlide;
   @Output() completed = new EventEmitter<void>();
 
   readonly formUrl = 'https://gramalote.com/formulario-contacto/';
   readonly trustedUrl: SafeResourceUrl;
+  protected audio?: HTMLAudioElement;
+  protected noAudio = false;
+  private audioInitialized = false;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, private logger: LoggerService) {
     this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.formUrl);
+  }
+
+  ngOnInit() {
+    this.initAudio();
+  }
+
+  ngOnChanges() {
+    if (!this.audioInitialized) {
+      this.initAudio();
+    }
+  }
+
+  private initAudio() {
+    if (this.slide?.audio && !this.audioInitialized) {
+      this.audioInitialized = true;
+      this.playAudio();
+    }
+  }
+
+  private playAudio() {
+    this.logger.debug('Playing audio for slide ', this.slide?.id);
+    this.noAudio = false;
+    this.audio = new Audio(this.slide!.audio);
+    this.audio.currentTime = 0;
+    this.audio.play();
+    this.audio.onended = () => {
+      this.noAudio = true;
+      this.logger.debug('Audio ended for slide ', this.slide?.id);
+    };
+  }
+
+  private stopAudio() {
+    if (this.audio) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.audio.src = '';
+      this.audio = undefined;
+      this.noAudio = true;
+    }
+  }
+
+  toggleAudio() {
+    if (!this.audio) return;
+    if (this.audio.paused) {
+      this.noAudio = false;
+      this.audio.play();
+    } else {
+      this.noAudio = true;
+      this.audio.pause();
+    }
   }
 
   onContinue() {
     this.completed.emit();
+  }
+
+  ngOnDestroy() {
+    this.stopAudio();
   }
 }
